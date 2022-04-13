d4b7f26f7cc2b0b927e31f275d51f38e205aa552
peterdesmet <peter.desmet@inbo.be>
2022-04-13 17:21:45 +0200

    Simplify CASE WHEN

--- sql/dwc_occurrence.sql
+++ sql/dwc_occurrence.sql
@@ -3,32 +3,27 @@ Created by Damiano Oldoni (INBO)
 */
 
 SELECT
-  o.'deelmonster_id'                          AS eventID,
+  o.'deelmonster_id'                    AS eventID,
   o.deelmonster_id || ':' || o.species_name_hash AS occurrenceID,
   CASE 
     WHEN o.'Macrofyt Naam' = 'Draadwier' THEN 'thread algae'
     WHEN o.'Macrofyt Naam' = 'Draadwier (submers)' THEN 'thread algae'
-    WHEN o.'Macrofyt Naam'  = 'Salix spp. (broad leaves)' THEN 'Salix'
-    WHEN o.'Macrofyt Naam'  = 'Salix spp. (small leaves)' THEN 'Salix'
-    WHEN o.'Macrofyt Naam'  = 'Nymphaea spec. (neofyt)' THEN 'Nymphaea'
-    WHEN o.'Macrofyt Naam'  = 'Salix fragilis L. (incl. kruisingen)' 
-      THEN 'Salix fragilis L.'
-    WHEN o.'Macrofyt Naam'  = 'Salix cinerea L. (incl. kruisingen)' 
-      THEN 'Salix cinerea L.'
-    WHEN o.'Macrofyt Naam' = 'Taraxacum Wiggers sect. Subvulgaria' AND 
-      o.'Macrofyt Auteur' = 'Christians.' 
+    WHEN o.'Macrofyt Naam' = 'Salix spp. (broad leaves)' THEN 'Salix'
+    WHEN o.'Macrofyt Naam' = 'Salix spp. (small leaves)' THEN 'Salix'
+    WHEN o.'Macrofyt Naam' = 'Nymphaea spec. (neofyt)' THEN 'Nymphaea'
+    WHEN o.'Macrofyt Naam' = 'Salix fragilis L. (incl. kruisingen)' THEN 'Salix fragilis L.'
+    WHEN o.'Macrofyt Naam' = 'Salix cinerea L. (incl. kruisingen)' THEN 'Salix cinerea L.'
+    WHEN o.'Macrofyt Naam' = 'Taraxacum Wiggers sect. Subvulgaria' AND o.'Macrofyt Auteur' = 'Christians.'
       THEN 'Taraxacum Wiggers sect. Subvulgaria Christians.'
-    WHEN o.'Macrofyt Naam' = 'Lysichiton americanus' AND 
-      o.'Macrofyt Auteur' = ' Hultén et St John x L. camtschatcensis (L.) Schott' 
+    WHEN o.'Macrofyt Naam' = 'Lysichiton americanus' AND o.'Macrofyt Auteur' = ' Hultén et St John x L. camtschatcensis (L.) Schott'
       THEN 'Lysichiton americanus × camtschatcensis'
     WHEN o.'Macrofyt Naam' = 'Trifolium' THEN 'Trifolium Tourn. ex L.'
-    -- replace 'groep' with 'group' in Macrofyt Naam
-    WHEN o.'Macrofyt Auteur' IS NOT NULL 
+    WHEN o.'Macrofyt Auteur' IS NOT NULL -- Replace 'groep' with 'group' in Macrofyt Naam
       THEN RTRIM(REPLACE(o.'Macrofyt Naam', 'groep', 'group')) || + ' ' || + o.'Macrofyt Auteur'
-    WHEN o.'Macrofyt Auteur' IS NULL 
+    WHEN o.'Macrofyt Auteur' IS NULL
       THEN RTRIM(REPLACE(o.'Macrofyt Naam', 'groep', 'group'))
-  END                                         AS scientificName,
-  'Plantae'                                   AS kingdom,
+  END                                   AS scientificName,
+  'Plantae'                             AS kingdom,
   CASE
     WHEN o.'Macrofyt Rang' = 'Species' THEN 'species'
     WHEN o.'Macrofyt Rang' = 'Genus' THEN 'genus'
@@ -36,32 +31,15 @@ SELECT
     WHEN o.'Macrofyt Rang' = 'Variety' THEN 'variety'
     WHEN o.'Macrofyt Rang' = 'Species group' THEN 'agg.'
     WHEN o.'Macrofyt Rang' = 'Species hybrid' THEN 'species'
-    -- for Taraxacum Wiggers sect. Subvulgaria Christians
-    WHEN o.'Macrofyt Rang' = 'Section' AND 
-      o.'Macrofyt Naam' = 'Taraxacum Wiggers sect. Subvulgaria' AND 
-      o.'Macrofyt Auteur' = 'Christians.' THEN 'genus'
-    -- all any other possible taxa with rank section if present
-    WHEN o.'Macrofyt Rang' = 'Section' AND 
-      o.'Macrofyt Naam' != 'Taraxacum Wiggers sect. Subvulgaria' THEN 'section'
+    WHEN o.'Macrofyt Rang' = 'Section' THEN 'section'
     WHEN o.'Macrofyt Rang' = 'Subgenus' THEN 'subgenus'
     WHEN o.'Macrofyt Rang' = 'Subspecies' THEN 'subspecies'
-    -- for Nymphaea spec. (neofyt)
-    WHEN o.'Macrofyt Rang' = 'Unknown' AND
-      o.'Macrofyt Naam' = 'Nymphaea spec. (neofyt)' THEN 'genus'
-    -- other taxa with rank Unknown if present
-    WHEN o.'Macrofyt Rang' = 'Unknown' AND
-      o.'Macrofyt Naam' != 'Nymphaea spec. (neofyt)' THEN 'unranked'
-    -- draadwier or draadwier (submers)
-    WHEN o.'Macrofyt Rang' = 'Functional group' AND
-      o.'Macrofyt Naam' LIKE 'Draadwier%' THEN 'functional group'
-    -- Salix spp. (broad leaves) or Salix spp. (small leaves)
-    WHEN o.'Macrofyt Rang' = 'Functional group' AND
-      o.'Macrofyt Naam' NOT LIKE 'Draadwier%' AND
-      o.'Macrofyt Naam' LIKE 'Salix spp.%' THEN 'genus'
-    -- 'Salix fragilis L. (incl. kruisingen)' or 'Salix cinerea L. (incl. kruisingen)'
-    WHEN o.'Macrofyt Rang' = 'Functional group' AND
-      o.'Macrofyt Naam' NOT LIKE 'Draadwier%' AND 
-      o.'Macrofyt Naam' NOT LIKE 'Salix spp.%' THEN 'species'
+    WHEN o.'Macrofyt Rang' = 'Unknown' AND o.'Macrofyt Naam' = 'Nymphaea spec. (neofyt)' THEN 'genus'
+    WHEN o.'Macrofyt Rang' = 'Unknown' THEN 'unranked'
+    WHEN o.'Macrofyt Rang' = 'Functional group' AND o.'Macrofyt Naam' LIKE 'Salix spp.%' THEN 'genus'
+    WHEN o.'Macrofyt Rang' = 'Functional group' AND o.'Macrofyt Naam' = 'Salix fragilis L. (incl. kruisingen)' THEN 'species'
+    WHEN o.'Macrofyt Rang' = 'Functional group' AND o.'Macrofyt Naam' = 'Salix cinerea L. (incl. kruisingen)' THEN 'species'
+    WHEN o.'Macrofyt Rang' = 'Functional group' THEN 'functional group'
     WHEN o.'Macrofyt Rang' = 'Species aggregate' THEN 'speciesAggregate'
   END                                         AS taxonRank,
   o.'Macrofyt Voorkeursnaam'                  AS vernacularName,
